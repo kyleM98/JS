760691aaee100a6d0703137293b3ea73cca3d0c9
kyle martin <kylemartin98@outlook.com>
2020-06-02 14:55:59 +0100

    unitTesting

--- JS-10 - Unit Tests in JavaScript/js/user/user-controller.test.js
+++ JS-10 - Unit Tests in JavaScript/js/user/user-controller.test.js
@@ -1,5 +1,6 @@
 const UserController = require("./user-controller");
 const User = require("./user");
+//const email = require("./email"); 
 
 const userController = new UserController();
 
@@ -15,5 +16,18 @@ test('remove user to userController', () => {
     userController.remove(user);
     expect(userController.users).not.toContain(user);
   });
+  /*
+1. Implement 1 test for the *add* function that verifies a user that is not in the users' list.
+2. Implement 1 test for the *remove* function that verifies a user that is not in the users' list.
+3. Implement 2 unit tests for the *findByEmail* function.
+4. Implement 2 unit tests for the *findById* function.
+*/
+test('add user to userController', () => {    
+  let user = new User(1234,"kyle", "kyle.m@fake.com");
+  userController.add(user);    
+  expect(userController.getUsers()).toContain(user);
+});
+  
 
+ 
 
